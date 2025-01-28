32a39572065414a64c1a6ea3a7eb1b9d6eda276e
IboTheBest <cybergenyus@gmail.com>
2025-01-28 17:55:43 +0500

    initial

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,15 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-})
+});
 
 export const getProduct = async (id) => {
-  const response = await api.get(`/products/${id}`)
-  return response.data
-}
-
+  try {
+    const { data } = await api.get(`/products/${id}`);
+    return data;
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    throw error; 
+  }
+};
